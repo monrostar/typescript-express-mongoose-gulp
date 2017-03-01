b64d81bd706dfd390b142c30664374a62da54a4e
Nitsenko <nitsenko94@gmail.com>
2017-03-01 08:18:13 +0300

    ! mongoose events

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,3 @@
-import * as errorHandler from "errorhandler";
 require("source-map-support").install();
 import config from "./config/index";
 
@@ -20,22 +19,56 @@ import { Index } from "./routes/";
 
 
 export class Server {
+
+  private _connection : mongoose.Connection;
+          assets : any;
+
   constructor(
     public app : express.Application,
     public assetMapPath : string = path.join(__dirname, "..", "assets", "webpack-assets.json")
   ) {}
 
-  assets : any;
+  public get connection() : mongoose.Connection {
+    return this._connection;
+  }
 
   private connectMongoose() {
 
     mongoose.Promise = Bluebird;
-    mongoose.connect(config.database, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log(`Connected to MongoDb ${config.database}`);
-    });
+
+    //connect to mongoose
+    this._connection = mongoose.createConnection(config.database);
+
+    // CONNECTION EVENTS
+    // When successfully connected
+
+      this._connection.on("connected", () => {
+        console.log(`Mongoose default connection open to  ${config.database}`);
+      });
+
+      // If the connection throws an error
+      this._connection.on("error", (parameters : { err : any }) => {
+        let err = parameters.err;
+        console.log(`Mongoose default connection error: ${err}`);
+      });
+
+      // When the connection is disconnected
+      this._connection.on("disconnected", () => {
+        console.log(`Mongoose default connection disconnected`);
+      });
+
+      // When the connection is reconnected
+      this._connection.on("reconnected", () => {
+        console.log(`Mongoose default connection is reconnected`);
+      });
+
+      // If the Node process ends, close the Mongoose connection
+      process.on("SIGINT", () => {
+        this._connection.close(() => {
+          console.log(`Mongoose default connection disconnected through app termination`);
+          process.exit(0);
+        });
+      });
 
   }
 
@@ -48,11 +81,13 @@ export class Server {
 
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    // TODO compression
     this.app.use(express.static(path.join(__dirname, "../public")));
     this.app.use("/client", express.static(path.join(__dirname, "../client")));
     this.app.use("/assets", express.static(path.join(__dirname, "../assets")));
     this.app.use("/css", express.static(path.join(__dirname, "../css")));
     this.app.use(favicon(path.join(__dirname, "../public/favicon.ico")));
+    // TODO express-session
 
   }
 
@@ -64,12 +99,12 @@ export class Server {
 
     //home page
     router.get("/", tokenMiddleware.checkToken.bind(tokenMiddleware), routes.index.bind(routes));
+    router.get("/api", tokenMiddleware.checkToken.bind(tokenMiddleware), routes.index.bind(routes));
 
     this.app.use(router);
 
     this.app.use(function (req : express.Request, res : express.Response, next : express.NextFunction) {
       res.status(404);
-
       // TODO: JSON response if requested in request
       res.render("error", {
         message: "Hmmmm, couldn't find that.",
@@ -110,10 +145,6 @@ export class Server {
 
   private errorsHandler() {
 
-    //lib
-    //this.app.use(errorHandler());
-
-    //custom
     if (this.app.get("env") === "development") {
       this.app.use((err : any, req : express.Request, res : express.Response, next : express.NextFunction) => {
         let { status = 500, message = "Server Error" } = err;
