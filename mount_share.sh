2b4f68b51ae2d5a92fc2126f3f372206c2f4a5be
Nitsenko Evgeniy <nitsenko94@gmail.com>
2017-03-27 10:58:01 +0200

    [task] Docker

---
+++ mount_share.sh
@@ -0,0 +1,1 @@
+docker-machine ssh default "cd /;sudo mkdir share; sudo mount -t vboxsf share /share
---
+++ run_compose.sh
@@ -0,0 +1,3 @@
+docker-compose -f docker/dev.yml stop
+docker-compose -f docker/dev.yml build
+docker-compose -f docker/dev.yml up
