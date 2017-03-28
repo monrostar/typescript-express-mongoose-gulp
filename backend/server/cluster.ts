03035d3f3d5246e7871c1f7d3c49ee900b14423f
Alexander <pond.aleksander@gmail.com>
2017-03-28 23:44:19 +0300

    [task] refactor logger

--- backend/server/cluster.ts
+++ backend/server/cluster.ts
@@ -5,6 +5,7 @@ import fs = require("fs");
 import { getServerConfigs } from "./config/env/index";
 import winston = require("winston");
 import Constants = require("./config/constants/constants");
+import Container = require("./container");
 
 class Cluster {
   private numCPUs : number;
@@ -32,30 +33,7 @@ class Cluster {
       return;
     }
 
-    let logDirectory = path.join(__dirname, "../logs");
-    // ensure log directory exists
-    if (fs.existsSync(logDirectory) === false) {
-      fs.mkdirSync(logDirectory);
-    }
-
-    let winstonLogger = new winston.Logger({
-      transports       : [ new (winston.transports.File)({
-        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
-        filename        : logDirectory + "/error-log.log",
-        handleExceptions: true,
-        json            : true,
-        maxsize         : 5242880, //5MB
-        maxFiles        : 5,
-        colorize        : false,
-        timestamp       : true
-      }), new (winston.transports.Console)({
-        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
-        handleExceptions: true,
-        json            : false,
-        colorize        : true
-      }) ], exitOnError: false
-    });
-
+    let consoleLogger = Container.ConsoleLogger;
 
     // Fork workers.
     let numberOfRequests = 0;
@@ -77,7 +55,7 @@ class Cluster {
 
       if (msg.cmd && msg.cmd === "notifyRequest") {
         numberOfRequests += 1;
-        winstonLogger.info(`Request count: ${numberOfRequests}`);
+        consoleLogger.log("info", `Request count: ${numberOfRequests}`);
       }
 
       switch (msg.type) {
@@ -85,7 +63,7 @@ class Cluster {
           console.log(msg.level, msg.data);
           break;
         case Constants.LOGGER_WINSTON:
-          winstonLogger.log(msg.level, msg.data);
+          consoleLogger.log(msg.level, msg.data);
           break;
         default:
           break;
@@ -95,18 +73,18 @@ class Cluster {
 
     cluster.on("online", (worker) => {
       //Если рабочий соединился с нами запишем это в лог!
-      winstonLogger.log("info", `Worker ${worker.id} running`);
+      consoleLogger.log("info", `Worker ${worker.id} running`);
     });
 
     cluster.on("listening", (worker, address) => {
-      winstonLogger.log("info", `Worker listening ${worker.process.pid} : port ${address.port}`);
+      consoleLogger.log("info", `Worker listening ${worker.process.pid} : port ${address.port}`);
     });
 
     cluster.on("disconnect", (worker, code, signal) => {
       // В случае отключения IPC запустить нового рабочего
 
       // запишем в лог отключение сервера, что бы разработчики обратили внимание.
-      winstonLogger.log("info", `Worker ${worker.id} died`);
+      consoleLogger.log("info", `Worker ${worker.id} died`);
 
       // Create a worker
       cluster.fork();
@@ -114,11 +92,11 @@ class Cluster {
 
     cluster.on("exit", (worker, code, signal) => {
       if (signal) {
-        winstonLogger.log("info", `worker was killed by signal: ${signal}`);
+        consoleLogger.log("info", `worker was killed by signal: ${signal}`);
       } else if (code !== 0) {
-        winstonLogger.log("info", `worker exited with error code: ${code}`);
+        consoleLogger.log("info", `worker exited with error code: ${code}`);
       } else {
-        winstonLogger.log("info", "worker success!");
+        consoleLogger.log("info", "worker success!");
       }
     });
   }
--- backend/server/config/middlewares/loggers/console-logger.ts
+++ backend/server/config/middlewares/loggers/console-logger.ts
@@ -2,19 +2,52 @@ import ILogger = require("./interfaces/i-logger");
 import cluster = require("cluster");
 import Constants = require("../../constants/constants");
 import * as winston from "winston";
+import path = require("path");
+import fs = require("fs");
 
 class ConsoleLogger implements ILogger {
+
+  protected logger: winston.Logger;
+
+  constructor() {
+    this.logger = this.winston;
+  }
+
+  protected get winston() : winston.Logger {
+
+    let logDirectory = path.join(__dirname, "../logs");
+    // ensure log directory exists
+    if (fs.existsSync(logDirectory) === false) {
+      fs.mkdirSync(logDirectory);
+    }
+
+    let winstonLogger = new winston.Logger({
+      transports       : [ new (winston.transports.File)({
+        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
+        filename        : logDirectory + "/error-log.log",
+        handleExceptions: true,
+        json            : true,
+        maxsize         : 5242880, //5MB
+        maxFiles        : 5,
+        colorize        : false,
+        timestamp       : true
+      }), new (winston.transports.Console)({
+        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
+        handleExceptions: true,
+        json            : false,
+        colorize        : true
+      }) ], exitOnError: false
+    });
+
+    return winstonLogger;
+
+  }
 
   public writeLogs(level : string, data : any) : void {
     if (!cluster.isMaster ) {
       process.send({ type: Constants.LOGGER_WINSTON, level, data });
     } else {
-      let logger = new winston.Logger({
-        transports       : [ , new (winston.transports.Console)({
-          levels: [ "debug", "info" ], handleExceptions: true, json: false, colorize: true
-        }) ], exitOnError: false
-      });
-      logger.log(level, data);
+      this.logger.log(level, data);
     }
   }
 
