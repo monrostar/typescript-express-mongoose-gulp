f07469d769fcc7b2dafc8597986e7e563ba98a04
Eugene Nitsenko <nitsenko94@gmail.com>
2017-04-06 01:13:11 +0300

    [task] Upgrade logger

--- backend/server/config/middlewares/loggers/console-logger.ts
+++ backend/server/config/middlewares/loggers/console-logger.ts
@@ -7,7 +7,7 @@ import fs = require("fs");
 
 class ConsoleLogger implements ILogger {
 
-  protected logger: winston.LoggerInstance;
+  protected logger : winston.LoggerInstance;
 
   constructor() {
     this.logger = ConsoleLogger.winston;
@@ -15,34 +15,44 @@ class ConsoleLogger implements ILogger {
 
   protected static get winston() : winston.LoggerInstance {
 
-    let logDirectory = path.join(__dirname, "../logs");
+    let logDirectory = path.join(__dirname, "../../../../logs");
     // ensure log directory exists
     if (fs.existsSync(logDirectory) === false) {
       fs.mkdirSync(logDirectory);
     }
 
     return new winston.Logger({
-      transports       : [ new (winston.transports.File)({
-        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
-        filename        : logDirectory + "/error-log.log",
-        handleExceptions: true,
-        json            : true,
-        maxsize         : 5242880, //5MB
-        maxFiles        : 5,
-        colorize        : false,
-        timestamp       : true
-      }), new (winston.transports.Console)({
-        levels          : process.env.NODE_ENV === "development" ? [ "debug" ] : [ "info", "error", "warn" ],
-        handleExceptions: true,
-        json            : false,
-        colorize        : true
-      }) ], exitOnError: false
+      transports: [
+        new (winston.transports.File)({
+          name            : "error-file",
+          levels          : [ "error", "warn" ],
+          filename        : logDirectory + "/error-log.log",
+          handleExceptions: true,
+          json            : true,
+          maxsize         : 5242880, //5MB
+          maxFiles        : 5,
+          colorize        : false,
+          timestamp       : true
+        }), new (winston.transports.File)({
+          name            : "info-file",
+          levels          : [ "info", "debug" ],
+          filename        : logDirectory + "/info-log.log",
+          handleExceptions: true,
+          json            : true,
+          maxsize         : 5242880, //5MB
+          maxFiles        : 1,
+          colorize        : false,
+          timestamp       : true
+        }), new (winston.transports.Console)({
+          levels: [ "info", "error", "warn", "debug" ], handleExceptions: true, json: false, colorize: true
+        })
+      ]
     });
 
   }
 
   public writeLogs(level : string, data : any) : void {
-    if (!cluster.isMaster ) {
+    if (!cluster.isMaster) {
       process.send({ type: Constants.LOGGER_WINSTON, level, data });
     } else {
       this.logger.log(level, data);
